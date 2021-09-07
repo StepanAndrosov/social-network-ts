81061528bacdf1e46e52d6e2fedb8a533d3c7655
Androsov-Stepan <stapan.news@yandex.ru>
2021-09-07 17:55:54 +0300

    app init

---
+++ src/component/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+ function Header() {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
